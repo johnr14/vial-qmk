842e7038aa28b70898fc56cf2694ec07f2af17a4
johnr14 <jonathanrioux@gmail.com>
2022-12-18 14:33:25 -0500

    debut changes made by keychron

--- keyboards/keychron/q5-old/q5.c
+++ keyboards/keychron/q5-old/q5.c
@@ -15,15 +15,20 @@
  */
 
 #include "q5.h"
+#include "rgb_matrix.h"
+
+// clang-format off
 
 const matrix_row_t matrix_mask[] = {
-    0b111111111111111111,
-    0b111111111111111111,
-    0b111111111111111111,
-    0b111111111111111111,
-    0b111111111111111111,
-    0b111111111111101111,
+    0b1111111111111111,
+    0b1111111111111111,
+    0b1111111111111111,
+    0b1111111111111111,
+    0b1111111111111111,
+    0b1111111111101111,
 };
+
+// clang-format on
 
 #ifdef DIP_SWITCH_ENABLE
 
@@ -37,16 +42,14 @@ bool dip_switch_update_kb(uint8_t index, bool active) {
     return true;
 }
 
-#endif  // DIP_SWITCH_ENABLE
+#endif
 
-#if defined(RGB_MATRIX_ENABLE) && (defined(CAPS_LOCK_LED_INDEX) || defined(NUM_LOCK_LED_INDEX))
+#if defined(RGB_MATRIX_ENABLE) && defined(CAPS_LOCK_LED_INDEX)
 
 bool process_record_kb(uint16_t keycode, keyrecord_t *record) {
-    if (!process_record_user(keycode, record)) {
-        return false;
-    }
+    if (!process_record_user(keycode, record)) { return false; }
     switch (keycode) {
-#    ifdef RGB_MATRIX_ENABLE
+#ifdef RGB_MATRIX_ENABLE
         case RGB_TOG:
             if (record->event.pressed) {
                 switch (rgb_matrix_get_flags()) {
@@ -64,33 +67,21 @@ bool process_record_kb(uint16_t keycode, keyrecord_t *record) {
                 rgb_matrix_enable();
             }
             return false;
-#    endif
+#endif
     }
     return true;
 }
 
- bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
-    if (!rgb_matrix_indicators_advanced_user(led_min, led_max)) { return false; }
+__attribute__((weak)) void rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
     // RGB_MATRIX_INDICATOR_SET_COLOR(index, red, green, blue);
-#    if defined(CAPS_LOCK_LED_INDEX)
+
     if (host_keyboard_led_state().caps_lock) {
         RGB_MATRIX_INDICATOR_SET_COLOR(CAPS_LOCK_LED_INDEX, 255, 255, 255);
     } else {
         if (!rgb_matrix_get_flags()) {
-            RGB_MATRIX_INDICATOR_SET_COLOR(CAPS_LOCK_LED_INDEX, 0, 0, 0);
+           RGB_MATRIX_INDICATOR_SET_COLOR(CAPS_LOCK_LED_INDEX, 0, 0, 0);
         }
     }
-#    endif // CAPS_LOCK_LED_INDEX
-#    if defined(NUM_LOCK_LED_INDEX)
-    if (host_keyboard_led_state().num_lock) {
-        RGB_MATRIX_INDICATOR_SET_COLOR(NUM_LOCK_LED_INDEX, 255, 255, 255);
-    } else {
-        if (!rgb_matrix_get_flags()) {
-            RGB_MATRIX_INDICATOR_SET_COLOR(NUM_LOCK_LED_INDEX, 0, 0, 0);
-        }
-    }
-#    endif // NUM_LOCK_LED_INDEX
-    return true;
 }
 
-#endif // RGB_MATRIX_ENABLE...
+#endif // CAPS_LOCK_LED_INDEX
